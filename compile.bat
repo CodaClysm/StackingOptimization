6599f4950743ab40f0e2caa01b5fe91608b789ca
Clayton McKinney <c.j.m.171202@gmail.com>
2020-11-19 12:07:31 -0500

    changed batch script to be more user friendly. No longer needs updated for each new file

--- compile.bat
+++ compile.bat
@@ -1,14 +1,4 @@
 ECHO starting compile...
-g++^
- src/Main.cpp^
- src/State.cpp^
- src/Shape.cpp^
- src/Individual.cpp^
- src/GeneticController.cpp^
- src/Environment.cpp^
- src/ControllerSettings.cpp^
- src/Chromosomes.cpp^
- src/AbsFeature.cpp^
- -o run.exe
+g++ src/*.cpp -o run.exe
 ECHO compile complete.
 PAUSE
